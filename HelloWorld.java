bd395a4275577bc76f114dad4bc6cb7ce0998abe
Ismael Garcia <tuxisma@gmail.com>
2023-07-27 16:51:23 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World - webhook!"); 
+        System.out.println("Hello, World - webhook! More updates"); 
     }
 }
